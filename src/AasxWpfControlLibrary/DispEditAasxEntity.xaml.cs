9bf5489f3cf7c101e493abd3f40e3a6f7657e88a
MichaelHoffmeisterFesto <miho@de.festo.com>
2020-12-24 13:20:45 +0100

    Update sources
    * plugins displayed again

--- src/AasxWpfControlLibrary/DispEditAasxEntity.xaml.cs
+++ src/AasxWpfControlLibrary/DispEditAasxEntity.xaml.cs
@@ -3246,6 +3246,8 @@ namespace AasxPackageExplorer
             // Dispatch
             //
 
+            var inhibitRenderStackToPanel = false;
+
             if (entity is VisualElementEnvironmentItem)
             {
                 var x = entity as VisualElementEnvironmentItem;
@@ -3364,6 +3366,8 @@ namespace AasxPackageExplorer
                 }
                 else
                 {
+                    // this is natively done; do NOT render Any UI to WPF
+                    inhibitRenderStackToPanel = true;
                 }
 
                 // show no panel nor scroll
@@ -3378,10 +3382,18 @@ namespace AasxPackageExplorer
 
 #if MONOUI
 #else
-            theMasterPanel.Children.Clear();
-            var spwpf = displayContext.GetOrCreateWpfElement(stack);
-            DockPanel.SetDock(spwpf, Dock.Top);
-            theMasterPanel.Children.Add(spwpf);
+            // render Any UI to WPF?
+            if (!inhibitRenderStackToPanel)
+            {
+                theMasterPanel.Children.Clear();
+                var spwpf = displayContext.GetOrCreateWpfElement(stack);
+                helper.ShowLastHighlights();
+                DockPanel.SetDock(spwpf, Dock.Top);
+                theMasterPanel.Children.Add(spwpf);
+            }
+
+            // keep the stack
+            lastRenderedRootElement = stack;
 #endif
 
             // return render hints
