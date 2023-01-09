3b81fcb788224887801a78ed933f335b1c209d9b
Matthias Freund <matthias.freund@festo.com>
2023-01-09 10:39:04 +0100

    Generate entities for components
    For each component in a harness description, this will generate an entity, link it to the main entity via a RelationshipElement and will also create a RelationshipElement with a fragment reference pointing to the component description within the VEC file.

--- src/AasxPluginVec/VecImporter.cs
+++ src/AasxPluginVec/VecImporter.cs
@@ -86,6 +86,7 @@ namespace AasxPluginVec
             this.vecSubmodel = null;
             this.bomSubmodels = new List<AdminShellV20.Submodel>();
             this.vecFile = ParseVecFile(fn);
+            this.vecFileSubmodelElement = null;
         }
 
         protected AdminShellPackageEnv packageEnv;
@@ -94,9 +95,11 @@ namespace AasxPluginVec
         protected AdminShell.Submodel vecSubmodel;
         protected List<AdminShell.Submodel> bomSubmodels;
         protected string fn;
-        private VecOptions options;
+        protected VecOptions options;
         protected LogInstance log;
         protected XDocument vecFile;
+        protected AdminShellV20.File vecFileSubmodelElement;
+
 
         protected void ImportVec()
         {
@@ -131,14 +134,36 @@ namespace AasxPluginVec
 
             // create the VEC file submodel element
             var file = new AdminShell.File();
-            file.value =
             file.idShort = "VEC";
             file.mimeType = "text/xml";
             file.value = localFilePath;
             vecSubmodel.AddChild(new AdminShellV20.SubmodelElementWrapper(file));
+            this.vecFileSubmodelElement = file;
         }
 
         protected void CreateBomSubmodel(XElement harnessDescription)
+        {
+            string harnessId = harnessDescription.Attribute(XName.Get("id"))?.Value ?? null;
+            string harnessDocumentNumber = harnessDescription.Element(XName.Get("DocumentNumber"))?.Value ?? null;
+
+            if (harnessId == null)
+            {
+                log.Error("Unable to determine ID of harness description!");
+                return;
+            }
+
+            if (harnessDocumentNumber == null)
+            {
+                log.Error("Unable to determine DocumentNumber of harness description!");
+                return;
+            }
+
+            var bomSubmodel = InitializeBomSubmodel();
+            var mainEntity = CreateMainEntity(bomSubmodel, harnessId, harnessDocumentNumber);
+            CreateComponentEntities(bomSubmodel, mainEntity, harnessDescription, harnessId);
+        }
+
+        private AdminShellV20.Submodel InitializeBomSubmodel()
         {
             // create the BOM submodel
             var bomSubmodel = new AdminShell.Submodel();
@@ -156,11 +181,104 @@ namespace AasxPluginVec
             env.Submodels.Add(bomSubmodel);
             aas.AddSubmodelRef(bomSubmodel.GetSubmodelRef());
 
+            return bomSubmodel;
+        }
+
+        private AdminShellV20.Entity CreateMainEntity(AdminShellV20.Submodel bomSubmodel, string harnessId, string harnessDocumentNumber)
+        {
             // create the main entity
             var mainEntity = new AdminShell.Entity();
-            mainEntity.idShort = harnessDescription.Element(XName.Get("DocumentNumber"))?.Value;
+            mainEntity.idShort = harnessDocumentNumber;
+            mainEntity.entityType = "SelfManagedEntity";
+            mainEntity.assetRef = this.aas.assetRef;
             bomSubmodel.Add(mainEntity);
 
+            // create the fragment relationship pointing to the DocumentVersion for the current harness
+            var fragmentRelationship = CreateVecRelationship(mainEntity, GetDocumentVersionFragment(harnessId));
+            mainEntity.AddChild(fragmentRelationship);
+
+            return mainEntity;
+        }
+
+        private void CreateComponentEntities(AdminShellV20.Submodel bomSubmodel, AdminShellV20.Entity mainEntity, XElement harnessDescription, string harnessId)
+        {
+            var compositionSpecifications = harnessDescription.Elements(XName.Get("Specification")).
+                            Where(spec => spec.Attribute(XName.Get("type", "http://www.w3.org/2001/XMLSchema-instance"))?.Value == "vec:CompositionSpecification");
+
+            foreach (var spec in compositionSpecifications)
+            {
+                var components = spec.Elements(XName.Get("Component"));
+                foreach (var component in components)
+                {
+                    CreateComponentEntity(bomSubmodel, mainEntity, component, harnessId);
+                }
+            }
+        }
+
+        private AdminShellV20.Entity CreateComponentEntity(AdminShellV20.Submodel bomSubmodel, AdminShellV20.Entity mainEntity, XElement component, string harnessId)
+        {
+            string componentId = component.Attribute(XName.Get("id"))?.Value ?? null;
+            string componentName = component.Element(XName.Get("Identification"))?.Value ?? null;
+
+            if (componentId == null)
+            {
+                log.Error("Unable to determine ID of component!");
+                return null;
+            }
+
+            if (componentName == null)
+            {
+                log.Error("Unable to determine name of component");
+                return null;
+            }
+
+            // create the entity
+            var componentEntity = new AdminShell.Entity();
+            componentEntity.idShort = componentName;
+            componentEntity.entityType = "SelfManagedEntity";
+            bomSubmodel.Add(componentEntity);
+
+            // create the fragment relationship pointing to the Component element for the current component
+            var fragmentRelationship = CreateVecRelationship(componentEntity, GetComponentFragment(harnessId, componentId));
+            componentEntity.AddChild(fragmentRelationship);
+
+            // create the relationship between the main and the component entity
+            mainEntity.AddChild(CreateBomRelationship(componentName, mainEntity, componentEntity));
+
+            return componentEntity;
+        }
+
+        protected AdminShellV20.SubmodelElementWrapper CreateVecRelationship(AdminShellV20.SubmodelElement first, string xpathToSecond)
+        {
+            var rel = new AdminShellV20.RelationshipElement();
+            rel.idShort = "VEC_Reference";
+            rel.semanticId = new AdminShellV20.SemanticId(new AdminShellV20.Key("ConceptDescription", false, "IRI", "http://arena2036.de/vws4ls/vec/VecPartReference/1/0"));
+
+            var second = this.vecFileSubmodelElement.GetReference();
+            second.Keys.Add(new AdminShellV20.Key("FragmentReference", true, "FragmentId", xpathToSecond));
+
+            rel.Set(first.GetReference(), second);
+
+            return new AdminShellV20.SubmodelElementWrapper(rel);
+        }
+
+        protected AdminShellV20.SubmodelElementWrapper CreateBomRelationship(string idShort, AdminShellV20.SubmodelElement first, AdminShellV20.SubmodelElement second)
+        {
+            var rel = new AdminShellV20.RelationshipElement();
+            rel.idShort = idShort;
+            rel.semanticId = new AdminShellV20.SemanticId(new AdminShellV20.Key("ConceptDescription", false, "IRI", "http://admin-shell.io/sandbox/CompositeComponent/General/IsPartOfForBOM/1/0"));
+            rel.Set(first.GetReference(), second.GetReference());
+            return new AdminShellV20.SubmodelElementWrapper(rel);
+        }
+
+        protected string GetDocumentVersionFragment(string documentVersionId)
+        {
+            return "//DocumentVersion[@id=" + documentVersionId + "]";
+        }
+
+        protected string GetComponentFragment(string documentVersionID, string componentVersionId)
+        {
+            return GetDocumentVersionFragment(documentVersionID) +  "//Component[@id=" + componentVersionId + "]";
         }
 
         protected XDocument ParseVecFile(string fn)
